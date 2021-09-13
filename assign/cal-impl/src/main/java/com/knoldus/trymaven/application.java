20b820f5394eaddee0ac5b3c19b4d4e5bd9b6487
ujjawal08 <ujjawal.singh@knoldus.com>
2021-09-13 13:48:18 +0530

    first commit

---
+++ assign/cal-impl/src/main/java/com/knoldus/trymaven/application.java
@@ -0,0 +1,10 @@
+package com.knoldus.trymaven;
+
+public class application {
+    public static class calculator{
+        public static void main(String[] args){
+            System.out.print("Enter two numbers: ");
+            System.out.print("\nEnter an operator (+, -, *, /): ");
+        }
+    }
+}
